e73e189b2273232465ae520ae5b9711ab8a77a5a
Egon Dohr <egondohr03@gmail.com>
2022-08-24 20:03:34 +0300

    basic functionality done

--- script.js
+++ script.js
@@ -33,12 +33,90 @@ const library = new Library()
 
 const addBookbtn = document.getElementById('addBtn')
 const bookPrompt = document.getElementById('bookPrompt')
-
+const overlay = document.getElementById('overlay')
+const submit = document.getElementById('submit')
+const bookForm = document.getElementById('bookForm')
+const booksGrid = document.getElementById('booksGrid')
 
 const openBookPrompt = () => {
-    bookPrompt.classList.toggle('active')
+    bookPrompt.classList.add('active')
+    overlay.classList.add('active')
+}
+
+const closeBookPrompt = () => {
+    bookPrompt.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+const createBook = () => {
+    const title = document.getElementById('title').value
+    const author = document.getElementById('author').value
+    const pages = document.getElementById('pages').value
+    const isRead = document.getElementById('isRead').value
+    return new Book(title, author, pages, isRead)
+}
+
+const updateBooksGrid = () => {
+    booksGrid.textContent = ''
+    for(let book of library.books) {
+        createBookCard(book)
+    }
+}
+
+const createBookCard = (book) => {
+    const bookCard = document.createElement('div')
+    const title = document.createElement('h3')
+    const author = document.createElement('p')
+    const pages = document.createElement('p')
+    const buttons = document.createElement('div')
+    const readBtn = document.createElement('p')
+    const removeBtn = document.createElement('p')
+
+    bookCard.classList.add('book-card')
+    buttons.classList.add('buttons')
+    readBtn.classList.add('btn')
+    removeBtn.classList.add('btn')
+    // readBtn.onclick = toggleRead
+    removeBtn.onclick = removeBook
+
+    title.textContent = book.title
+    author.textContent = book.author
+    pages.textContent = book.pages
+    removeBtn.textContent = 'Remove'
+    if(book.isRead) {
+        readBtn.textContent = 'Read'
+    }
+    else {
+        readBtn.textContent = 'Not Read'
+    }
+    bookCard.appendChild(title)
+    bookCard.appendChild(author)
+    bookCard.appendChild(pages)
+    buttons.appendChild(readBtn)
+    buttons.appendChild(removeBtn)
+    bookCard.appendChild(buttons)
+    booksGrid.appendChild(bookCard)
+}
+const addBook = (e) => {
+    e.preventDefault()
+    const newBook = createBook()
+    if(library.inLibrary(newBook))
+        return
+    else {
+        library.addBook(newBook)
+        updateBooksGrid()
+    }
+    closeBookPrompt()
+}
+const removeBook = (e) => {
+    console.log(e)
+    const title = e.target.parentNode.parentNode.firstChild.textContent
+    console.log(title)
+    library.removeBook(title)
+    updateBooksGrid()
 }
 
 addBookbtn.onclick = openBookPrompt
-
+bookForm.onsubmit = addBook
+overlay.onclick = closeBookPrompt
 
